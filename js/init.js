52920a3f0f6c2c110bc26140aac06e3822091009
Sergey Zhmailo <sergey.seoz@gmail.com>
2018-03-24 20:04:17 +0200

    add slider, footer

--- js/init.js
+++ js/init.js
@@ -2,4 +2,11 @@ $( function() {
     // Init tabs
     $( "#tabs" ).tabs();
     $( "#portfolio-tabs" ).tabs();
+
+    //init slik carousel
+    $('.reviews-carousel').slick({      
+        dots: true,
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',
+    });
 } );
