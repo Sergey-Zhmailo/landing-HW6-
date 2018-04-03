c1e94bd64a9643ccf210514dc2349def5aa487c5
Sergey Zhmailo <sergey.seoz@gmail.com>
2018-04-03 13:28:15 +0300

    add adaptiv + lightbox

--- js/init.js
+++ js/init.js
@@ -3,10 +3,15 @@ $( function() {
     $( "#tabs" ).tabs();
     $( "#portfolio-tabs" ).tabs();
 
-    //init slik carousel
+    //init slick carousel
     $('.reviews-carousel').slick({      
         dots: true,
         prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
         nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>',
+    });
+    // init slick nav mrnu
+    $('.header-nav').slicknav({
+       appendTo: '.header .container-fluid',
+       label: ''
     });
 } );
