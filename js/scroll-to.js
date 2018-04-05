00f70e3485ffca675985b798ef071fe9cb7689dc
Sergey Zhmailo <sergey.seoz@gmail.com>
2018-04-05 11:57:40 +0300

    add scroll fix

--- js/scroll-to.js
+++ js/scroll-to.js
@@ -1,14 +1,18 @@
 ;(function($) {
-	// Scroll to element
-	const scrollBtn = $('[data-scroll-to]');
+	$(window).on('load', function(){
+		// Scroll to element
+		const scrollBtn = $('[data-scroll-to]');
 
-	function onScroll(e) {
-	    e.preventDefault();
-	    let target = $(this).attr('data-scroll-to');
-	    let dist = $(target).offset().top;
-	    $('html, body').animate({scrollTop: dist}, 1000, 'swing');
-	}
+		function onScroll(e) {
+		    e.preventDefault();
 
-	scrollBtn.on('click', onScroll);
+		    // Close slick nav
+		    $('.header-nav').slicknav('close');
+		    let target = $(this).attr('data-scroll-to');
+		    let dist = $(target).offset().top;
+		    $('html, body').animate({scrollTop: dist}, 1000, 'swing');
+		}
 
+		scrollBtn.on('click', onScroll);
+	})
 })(jQuery);
